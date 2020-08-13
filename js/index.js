079f853f7e3dd1d7f7580187e0ead630d0ceb52e
Brett Cunningham <brett.cunningham@sbevision.com>
2020-08-12 20:43:56 -0400

    implemented delete functionality

--- js/index.js
+++ js/index.js
@@ -1,10 +1,36 @@
+// how do you set the attribute of a element you just created?
+// how do you append a child to an element
+// how do you get the text from an input field?
+// how do you add a class to an element you just created?
+// Order of appending matters
+// how do I get an image really quick?
+
 const addToDo = () => {
     console.log('does this button work')
     const inputTodo = document.getElementById('enterToDo');
     console.log('this is the text from the input field', inputTodo.value)
     const newTodo = document.createElement('li')
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('deleteButton')
+    deleteButton.setAttribute('onclick', 'deleteToDo(event)');
+    const deleteImg = document.createElement('img')
+    deleteImg.setAttribute('src', 'https://freepikpsd.com/wp-content/uploads/2019/10/icon-delete-png-4-Transparent-Images.png')
+    deleteImg.classList.add('deleteImg');
+    
+    deleteButton.appendChild(deleteImg);
+    
     var textnode = document.createTextNode(inputTodo.value)
+    
     const toDoList = document.getElementById('toDoList')
     newTodo.appendChild(textnode)
+    newTodo.appendChild(deleteButton)
+
     toDoList.appendChild(newTodo)
+}
+
+// how do you know which todo deletebutton was clicked on in order to remove
+// the correct one?
+const deleteToDo = (event) => {
+    console.log('delete button working', event.target)
+    event.target.parentElement.parentElement.remove()
 }
